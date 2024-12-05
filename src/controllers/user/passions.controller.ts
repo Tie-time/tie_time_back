04c1f6321710cf8538d436c6041ed1aba08afe4a
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-12-05 10:58:05 +0100

    feat(Passion): Update get passion return for user gh-17

--- src/controllers/user/passions.controller.ts
+++ src/controllers/user/passions.controller.ts
@@ -1,16 +1,42 @@
 import express, { Request, Response } from "express";
 import * as service from "../../services/passions.service";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import { RoleEnum } from "../../enums/RoleEnum";
+import { PassionsFilter } from "../../types/passions/filters/passions.filter";
+import { getDateWithoutTime } from "../../helpers/date/date.helpers";
+import { User } from "../../models/User";
 
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  try {
-    const passions = await service.getPassions();
+router.get(
+  "/",
+  authMiddleware({ roles: [RoleEnum.USER, RoleEnum.ADMIN] }),
+  async (req: Request, res: Response) => {
+    try {
+      const query = req.query;
 
-    res.status(200).send(passions);
-  } catch (error: any) {
-    res.status(500).send({ error: error.message });
+      if (!query.date) {
+        throw new Error("Date manquante");
+      }
+
+      const passionsFilter: PassionsFilter = {
+        date: new Date(query.date as string),
+      };
+
+      const formattedDate: Date = getDateWithoutTime(passionsFilter.date);
+
+      const userConnected = req.userConnected as User;
+
+      const passions = await service.getMyPassionsWithCheckedStatusByDate(
+        userConnected.id,
+        formattedDate
+      );
+
+      res.status(200).send(passions);
+    } catch (error: any) {
+      res.status(500).send({ error: error.message });
+    }
   }
-});
+);
 
 export default router;
--- src/services/passions.service.ts
+++ src/services/passions.service.ts
@@ -1,8 +1,36 @@
 import { AppDataSource } from "../database/data-source";
 import { Passion } from "../models/Passion";
+import { PassionCheckedBy } from "../models/PassionCheckedBy";
 
 const PassionRepository = AppDataSource.getRepository(Passion);
 
 export const getPassions = async (): Promise<Passion[]> => {
   return await PassionRepository.find();
+};
+
+export const getMyPassionsWithCheckedStatusByDate = async (
+  userId: string,
+  date: Date
+) => {
+  return await PassionRepository.createQueryBuilder("passion")
+    .leftJoin(
+      PassionCheckedBy,
+      "pcb",
+      "pcb.passion = passion.id AND pcb.checked_by = :userId AND pcb.date = :date",
+      { userId, date }
+    )
+    .select([
+      "passion.id as id",
+      "passion.label as label",
+      "passion.icon_url as icon_url",
+      "CASE WHEN pcb.id IS NOT NULL THEN true ELSE false END as is_checked",
+    ])
+    .orderBy("passion.id", "ASC")
+    .getRawMany()
+    .then((results) =>
+      results.map((result) => ({
+        ...result,
+        is_checked: result.is_checked === 1,
+      }))
+    );
 };
