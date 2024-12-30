08612ad4147c7ae79c750c0936206b75fb5c5dbb
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-12-30 13:32:35 +0100

    feat(Rates): Add update rate endpoint gh-26

--- src/controllers/user/rates.controller.ts
+++ src/controllers/user/rates.controller.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "../../middlewares/auth.middleware";
 import { User } from "../../models/User";
 import { getDateWithoutTime } from "../../helpers/date/date.helpers";
 import { RatesFilter } from "../../types/rates/filters/rates.filter";
+import { HttpError } from "../../errors/HttpError";
 
 const router = express.Router();
 
@@ -45,7 +46,7 @@ router.get(
       const query = req.query;
 
       if (!query.date) {
-        throw new Error("Date manquante");
+        throw new HttpError(500, "Date manquante");
       }
 
       const ratesFilter: RatesFilter = {
@@ -64,6 +65,50 @@ router.get(
       res.status(200).send(rates);
     } catch (error: any) {
       res.status(500).send({ error: error.message });
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  authMiddleware({ roles: [RoleEnum.USER, RoleEnum.ADMIN] }),
+  async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const possibleFields = ["score"];
+      const fields = Object.keys(req.body);
+      const isValidOperation = fields.every((field) =>
+        possibleFields.includes(field)
+      );
+
+      if (!isValidOperation) {
+        throw new Error(
+          `Champs invalides, les champs possible sont: ${possibleFields.join(
+            ", "
+          )}`
+        );
+      }
+
+      const rateData = req.body;
+
+      const userConnected = req.userConnected as User;
+
+      const rate = await rateService.getMyRateById(userConnected, id);
+
+      if (!rate) {
+        throw new HttpError(404, "Note non trouvée");
+      }
+
+      const rateUpdated = await rateService.updateRate(id, rateData);
+
+      if (!rateUpdated) {
+        throw new HttpError(500, "Erreur lors de la mise à jour de la note");
+      }
+
+      res.status(200).send({ success: "Note mise à jour avec succès" });
+    } catch (error: any) {
+      const statusCode = error.statusCode || 500;
+      res.status(statusCode).send({ error: error.message });
     }
   }
 );
--- src/services/rate.service.ts
+++ src/services/rate.service.ts
@@ -1,9 +1,29 @@
+import { UpdateResult } from "typeorm";
 import { AppDataSource } from "../database/data-source";
 import { Rate } from "../models/Rate";
+import { User } from "../models/User";
 
 const rateRepository = AppDataSource.getRepository(Rate);
 
 export const createRate = async (data: Rate): Promise<Rate> => {
   const newRate = await rateRepository.save(data);
   return newRate;
+};
+
+export const getMyRateById = async (
+  created_by: User,
+  id: string
+): Promise<Rate | null> => {
+  const rate = await rateRepository.findOne({
+    where: { created_by: { id: created_by.id }, id },
+  });
+  return rate;
+};
+
+export const updateRate = async (
+  id: string,
+  rateData: Partial<Rate>
+): Promise<UpdateResult> => {
+  const updatedRate = await rateRepository.update({ id }, rateData);
+  return updatedRate;
 };
